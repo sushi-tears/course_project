dddcdb45186aeb6ed9c47ad5933bec78c5d0893c
levyjake808@gmail.com <levyjake808@gmail.com>
2022-08-28 19:06:16 -0400

    final commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./Home.css";
 import splash from "./splash.jpg";
 
-export default function Networking() {
+export default function Home() {
   return (
-    <div id="splash">
-      <img alt="splash" src={splash} />
+    <div id="container">
+      <img id="splash" alt="splash" src={splash} />
+      <div id="search">
+        <p>Looking for something specific?</p>
+        <input id="searchBar" placeholder="Search..." />
+        <button id="searchButton">GO!</button>
+      </div>
     </div>
   );
 }
